72b250eadd1a5442a3edc63f95d2d9e4e94db363
Rami Pinku <rpinku77@gmail..com>
2025-10-30 14:50:42 +0200

    moving distilgpt2_conda from qwak to jfrog

---
+++ distilgpt2_conda/test_live_model.py
@@ -0,0 +1,32 @@
+import argparse
+import pandas as pd
+from frogml_inference import RealTimeClient
+
+
+def main(model_id):
+
+    # Define the columns
+    columns = ["prompt"]
+
+    # Define the data
+    data = [["what is love?"]]
+
+    input_ = pd.DataFrame(data, columns=columns)    
+    client = RealTimeClient(model_id=model_id)
+    
+    response = client.predict(input_)
+    print(response)
+
+
+"""
+USAGE:
+
+>> python main/test_live_model.py <your_model_id>
+
+"""
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Predict using the following JFrog ML model-id.')
+    parser.add_argument('model_id', type=str, help='The JFrog ML model ID to call for prediction.')
+
+    args = parser.parse_args()
+    main(args.model_id)
